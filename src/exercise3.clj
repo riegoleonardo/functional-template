071b98f52cd1f08a2062be8baf4f2bad05063f18
Leonardo Riego <leonardo.riego@despegar.com>
2020-05-18 23:29:34 -0300

    Added resolution for exercise 3

--- src/exercise3.clj
+++ src/exercise3.clj
@@ -3,9 +3,9 @@
 (defn fibonacci
   "Calculates the fibonacci result fo the given input value."
   [x]
-  (throw (Exception. "Not implemented."))
+  (loop [counter x current 1 previous 1]
+    (cond
+      (= 0 x) 0
+      (<= counter 2) current
+      :default (recur (dec counter) (+ current previous) current)))
 )
-
-
-
-
